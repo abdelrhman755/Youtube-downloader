5910ffcd85141ab680f09d6c9b3fb82c94961e8a
Abdelrhman <abdelrazkforman966@gmail.com>
2026-05-19 15:49:39 +0300

    changing small things in the html code and the css code, and writing the conditions of the download link in java script

---
+++ js.js
@@ -0,0 +1,20 @@
+const input = document.getElementById ("input");
+const button = document.getElementById ("download-button");
+const message = document.getElementById ("message");
+
+button.addEventListener ("click", () => {
+    const videoURL = input.value;
+    if (videoURL ==="") {
+        message.innerHTML = "please enter a youtube video link";
+        message.style.color = "red";
+        return;
+    
+    }
+    if(!videoURL.includes("youtube.com") && !videoURL.includes("youtu.be")){
+        message.innerHTML = "invalid link, please enter a valid youtube video link";
+        message.style.color = "red";
+        return;
+    }
+    message.innerHTML = "Link is valid and ready for download";
+    message.style.color = "green";
+});
